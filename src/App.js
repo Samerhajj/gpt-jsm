1117ef61f9840d066fc298b5ae7da52d404c2bba
shadi haj <samerhaj44@gmail.com>
2023-06-14 03:52:06 +0300

    Added css and appjs adjustment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React from 'react';
 
+import {Footer,Blog,Possibility,Features,WhatGPT4,Header} from './containers';
+import {CTA,Brand,Navbar} from './components';
+import './App.css';
 function App() {
   return (
-   <div>
-    <h1>GPT-3</h1>
+   <div className='App'>
+    <div className='gradient_bg'>
+      <Navbar/>
+      <Header/>
+    </div>
+    <Brand/>
+    <WhatGPT4/>
+    <Features/>
+    <Possibility/>
+    <CTA/>
+    <Blog/>
+    <Footer/>
     </div>
   );
 }
